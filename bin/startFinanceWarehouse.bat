0772cceb162ac68a4031688a81e59bd204e90383
MichaelGCox <Michael.Cox@SRB-ES.com>
2014-08-14 06:59:24 -0700

    Changed proeprties and database values.

--- bin/startFinanceWarehouse.bat
+++ bin/startFinanceWarehouse.bat
@@ -2,6 +2,6 @@ SET PDI_HOME=C:\Users\MichaelC\Utilities\pdi-ce-5.1.0.0-752\data-integration
 SET SRB_WAREHOUSE_HOME=C:\\Users\\MichaelC\\Development\\github\\SRBWarehouse
 SET SRB_WAREHOUSE_JOB=FinanceWarehouse/Job/startFinanceWarehouse
 SET SRB_WAREHOUSE_PROPERTIES=finance.properties
-SET SRB_WAREHOUSE_LOG=financeWarehouse.log
+SET SRB_WAREHOUSE_LOG=finance.log
 
 %PDI_HOME%\kitchen.bat /rep:KettleRepo /job:%SRB_WAREHOUSE_JOB% /log:%SRB_WAREHOUSE_HOME%\log\%SRB_WAREHOUSE_LOG% /param:SRB_PROPERTIES_FILE_PARAM=%SRB_WAREHOUSE_HOME%\\bin\\%SRB_WAREHOUSE_PROPERTIES%

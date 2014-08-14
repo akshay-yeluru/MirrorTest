9f4faea640a9537b9b5f78e97b9036cb640679e6
MichaelGCox <Michael.Cox@SRB-ES.com>
2014-08-14 08:36:56 -0700

    Renamed.

--- bin/startTeacPayrollWarehouse.bat
+++
@@ -1,7 +0,0 @@
-SET PDI_HOME=C:\Users\MichaelC\Utilities\pdi-ce-5.1.0.0-752\data-integration
-SET SRB_WAREHOUSE_HOME=C:\\Users\\MichaelC\\Development\\github\\SRBWarehouse
-SET SRB_WAREHOUSE_JOB=PayrollWarehouse/Job/startPayrollWarehouse
-SET SRB_WAREHOUSE_PROPERTIES=payrollTeac.properties
-SET SRB_WAREHOUSE_LOG=payrollTeacWarehouse.log
-
-%PDI_HOME%\kitchen.bat /rep:KettleRepo /job:%SRB_WAREHOUSE_JOB% /log:%SRB_WAREHOUSE_HOME%\log\%SRB_WAREHOUSE_LOG% /param:SRB_PROPERTIES_FILE_PARAM=%SRB_WAREHOUSE_HOME%\\bin\\%SRB_WAREHOUSE_PROPERTIES%

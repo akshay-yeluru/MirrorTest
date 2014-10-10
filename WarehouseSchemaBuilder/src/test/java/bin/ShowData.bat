c2704ab1843fcdd48248185993b2b0ed8016f621
MichaelGCox <Michael.Cox@SRB-ES.com>
2014-10-10 13:02:48 -0700

    Made some changes.

--- WarehouseSchemaBuilder/src/test/java/bin/ShowData.bat
+++ WarehouseSchemaBuilder/src/test/java/bin/ShowData.bat
@@ -1,1 +1,1 @@
-java -mx32M -cp ../target/SRBWarehouseSchemaBuilder-0.0.1-SNAPSHOT.jar;../lib/jtds-1_2.jar;../lib/log4j-1_2_12.jar;../lib/vortex_2006_02_28.jar com.tscsoftware.warehouse.VortexVersion finance.properties
+This is a simple file.

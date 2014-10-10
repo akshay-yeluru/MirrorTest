e89eedc58618e23a8e7836cfbf324f0d8b6c4268
MichaelGCox <Michael.Cox@SRB-ES.com>
2014-10-10 13:04:29 -0700

    Added real comment to ShowVortexVersion.bat

--- WarehouseSchemaBuilder/src/test/java/bin/ShowData.bat
+++ WarehouseSchemaBuilder/src/test/java/bin/ShowData.bat
@@ -1,2 +1,3 @@
 This is a simple file.
 This file now has another change.
+This is not a change.  This is just a tribute.
--- WarehouseSchemaBuilder/src/test/java/bin/ShowVortexVersion.bat
+++ WarehouseSchemaBuilder/src/test/java/bin/ShowVortexVersion.bat
@@ -1,2 +1,2 @@
-REM Added some comments.
+REM This class provides a method to get the version number from vortex jar file.
 java -mx32M -cp ../target/SRBWarehouseSchemaBuilder-0.0.1-SNAPSHOT.jar;../lib/jtds-1_2.jar;../lib/log4j-1_2_12.jar;../lib/vortex_2006_02_28.jar com.tscsoftware.warehouse.VortexVersion finance.properties

0a2cc87c62e9fe431095181fde7e511d1525fd35
MichaelGCox <Michael.Cox@SRB-ES.com>
2014-10-10 11:49:16 -0700

    Moved file from com.tscsoftware.warehouse to com.tscsoftware.warehouse.db.vortex

---
+++ WarehouseSchemaBuilder/src/main/java/com/tscsoftware/warehouse/db/vortex/VortexVersion.java
@@ -0,0 +1,59 @@
+package com.tscsoftware.warehouse.db.vortex;
+
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class VortexVersion {
+
+	static Logger _logger = Logger.getLogger(VortexVersion.class);
+
+	public static void main(String[] args) {
+		try {
+
+			// get properties file or die.
+			if (args.length != 1) {
+
+				// inform of proper usage
+				String err = "Incorrect usage.  Include the configuration properties file as the first argument.";
+				_logger.error(err);
+				System.exit(0);
+			}
+
+			BasicConfigurator.configure();
+
+			Properties props = new Properties();
+			try {
+
+				FileInputStream in = new FileInputStream(args[0]);
+				props.load(in);
+				in.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			String sourceDriver = props.getProperty("SOURCE_DRIVER");
+			String sourceConnectionURL = props
+					.getProperty("SOURCE_CONNECTION_URL");
+
+			Connection conn = null;
+			DatabaseMetaData dmd;
+
+			Class.forName(sourceDriver);
+			conn = DriverManager.getConnection(sourceConnectionURL);
+			dmd = conn.getMetaData();
+
+			_logger.info("getDriverVersion: " + dmd.getDriverVersion());
+
+			if (conn != null)
+				conn.close();
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+
+}

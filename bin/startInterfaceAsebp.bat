e586d440b0d4c926687230e627feec1dae80fabe
MichaelGCox <Michael.Cox@SRB-ES.com>
2015-05-19 11:20:45 -0700

    Added example properties for ASEBP interface.

---
+++ bin/startInterfaceAsebp.bat
@@ -0,0 +1,6 @@
+call setPaths.bat
+SET SRB_WAREHOUSE_JOB=Interfaces/Asebp/Job/startAsebp
+SET SRB_WAREHOUSE_PROPERTIES=asebp.properties
+SET SRB_WAREHOUSE_LOG=asebp.log
+
+%PDI_HOME%\kitchen.bat /rep:KettleRepo /job:%SRB_WAREHOUSE_JOB% /log:%SRB_WAREHOUSE_CLIENT%\log\%SRB_WAREHOUSE_LOG% /param:SRB_PROPERTIES_FILE_PARAM=%SRB_WAREHOUSE_CLIENT%\\bin\\%SRB_WAREHOUSE_PROPERTIES%

fe6aa7118e327d9ab539371721b1a6b165179e34
William Bain <william.bain@srb-es.com>
2015-10-13 13:12:33 -0700

    Change for ticket 129905 - show position description instead of code.

--- bin/setPaths.bat
+++ bin/setPaths.bat
@@ -1,4 +1,4 @@
-SET PDI_HOME=C:\Users\MichaelC\Utilities\pdi-ce-5.1.0.0-752\data-integration
-SET SRB_WAREHOUSE_HOME=C:\\Users\\MichaelC\\Development\\github\\SRBWarehouse
-SET SRB_WAREHOUSE_CLIENT=C:\\Users\\MichaelC\\Development\\github\\SRBWarehouse
-SET JAVA_LIB_CLASSPATH=%SRB_WAREHOUSE_HOME%\lib\jtds-1.2.jar;%SRB_WAREHOUSE_HOME%\lib\log4j-1.2.12.jar;%SRB_WAREHOUSE_HOME%\lib\SRBWarehouseSchemaBuilder-0.0.1-SNAPSHOT.jar;%SRB_WAREHOUSE_HOME%\lib\vortex-1.1.0.36-140921.jar
+SET PDI_HOME=C:\Pentaho Data Integration
+SET SRB_WAREHOUSE_HOME=C:\\LocalRepositories\\SRBWarehouse
+SET SRB_WAREHOUSE_CLIENT=C:\\LocalRepositories\\SRBWarehouse
+SET JAVA_LIB_CLASSPATH=%SRB_WAREHOUSE_HOME%\lib\jtds-1.2.jar;%SRB_WAREHOUSE_HOME%\lib\log4j-1.2.12.jar;%SRB_WAREHOUSE_HOME%\lib\SRBWarehouseSchemaBuilder-0.0.1-SNAPSHOT.jar;%SRB_WAREHOUSE_HOME%\lib\vortex-1.1.0.37-141031.jar

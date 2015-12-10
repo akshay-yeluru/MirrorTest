0c8d857776c5f1b509891387b0c06d10debac89d
MichaelGCox <Michael.Cox@SRB-ES.com>
2015-12-10 13:33:07 -0800

    Small changes to make deployment a little easier.

--- bin/setPaths.bat
+++ bin/setPaths.bat
@@ -1,4 +1,4 @@
-SET PDI_HOME=C:\Pentaho Data Integration
-SET SRB_WAREHOUSE_HOME=C:\\LocalRepositories\\SRBWarehouse
-SET SRB_WAREHOUSE_CLIENT=C:\\LocalRepositories\\SRBWarehouse
-SET JAVA_LIB_CLASSPATH=%SRB_WAREHOUSE_HOME%\lib\jtds-1.2.jar;%SRB_WAREHOUSE_HOME%\lib\log4j-1.2.12.jar;%SRB_WAREHOUSE_HOME%\lib\SRBWarehouseSchemaBuilder-0.0.1-SNAPSHOT.jar;%SRB_WAREHOUSE_HOME%\lib\vortex-1.1.0.37-141031.jar
+SET PDI_HOME=C:\utilities\pdi-ce-5.1.0.0-752\data-integration
+SET SRB_WAREHOUSE_HOME=C:\\utilities\\warehousePDI\\SRBWarehouse
+SET SRB_WAREHOUSE_CLIENT=C:\\utilities\\warehousePDI\\live
+SET JAVA_LIB_CLASSPATH=%SRB_WAREHOUSE_HOME%\lib\jtds-1.2.jar;%SRB_WAREHOUSE_HOME%\lib\log4j-1.2.12.jar;%SRB_WAREHOUSE_HOME%\lib\SRBWarehouseSchemaBuilder-0.0.1-SNAPSHOT.jar;%SRB_WAREHOUSE_HOME%\lib\vortex-2005.08.30.jar

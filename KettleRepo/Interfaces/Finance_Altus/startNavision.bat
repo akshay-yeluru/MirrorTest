bf0cc9987b85119a5ccf128d3b2ccdf4860378f0
Don Goudreau <don.goudreau@srb-es.com>
2016-10-31 13:06:02 -0600

    RE

--- KettleRepo/Interfaces/Finance_Altus/startNavision.bat
+++
@@ -1,6 +0,0 @@
-call setPaths.bat
-SET SRB_WAREHOUSE_JOB=Interfaces/Finance_GreatPlains/Job/startGreatPlains
-SET SRB_WAREHOUSE_PROPERTIES=GreatPlains.properties
-SET SRB_WAREHOUSE_LOG=GreatPlains.log
-
-%PDI_HOME%\kitchen.bat /rep:KettleRepo /job:%SRB_WAREHOUSE_JOB% /log:%SRB_WAREHOUSE_CLIENT%\log\%SRB_WAREHOUSE_LOG% /param:SRB_PROPERTIES_FILE_PARAM=%SRB_WAREHOUSE_CLIENT%\\bin\\%SRB_WAREHOUSE_PROPERTIES%
